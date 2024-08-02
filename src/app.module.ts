67d492f60e5bcc80e8888525f19a32998ea4d654
woorim02 <99327851+woorim02@users.noreply.github.com.>
2024-08-03 00:31:50 +0900

    add nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [ConfigModule.forRoot(
+    {
+      envFilePath: (process.env.NODE_ENV === 'development') ? '.dev.env' : '.env'
+    }
+  ), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
