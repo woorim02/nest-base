9e3cb41199f9b99d05e68a3f00885a45842ae7bd
woorim02 <99327851+woorim02@users.noreply.github.com.>
2024-08-03 20:59:39 +0900

    update: User 엔티티 세부사항 추가

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity({ name: 'user' })
+export class User {
+    @PrimaryColumn()
+    id: number;
+
+    @Column({ length: 30, nullable: false })
+    userName: string;
+
+    @Column({ length: 60, nullable: false })
+    email: string;
+
+    @Column({ length: 255, nullable: false })
+    encPassword: string;
+
+    @Column('simple-array', { default: 'user', nullable: false })
+    roles: string[];
+}
