f8f09e8908bdd8452d153c4aa7f4bdeee3b82322
woorim02 <99327851+woorim02@users.noreply.github.com.>
2024-08-04 14:28:56 +0900

    add: TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: process.env.NODE_ENV === 'development' ? '.dev.env' : '.env',
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'nest-base',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false
     }),
     UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
