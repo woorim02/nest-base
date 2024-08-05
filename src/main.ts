cf3081a5cdb5de7ba7ac51acc331650ebd165da3
woorim02 <99327851+woorim02@users.noreply.github.com.>
2024-08-05 10:57:21 +0900

    swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,14 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+  .setTitle("nest-base")
+  .setDescription("nest Boilerplate")
+  .setVersion("1.0")
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("swagger/index.html", app, document);
+  
   // start program
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('PORT') || 3000);
